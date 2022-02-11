bb9503266f04ebdea70ad228cf01b4b94a3637cf
angelo-loria <angelosloria@gmail.com>
2022-02-11 17:20:24 -0600

    login test

--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -1,7 +1,42 @@
+import DashboardPage from '../pageobjects/dashboard.page'
 import HomePage from '../pageobjects/home.page'
+import LoginPage from '../pageobjects/login.page'
+import ResetPasswordPage from '../pageobjects/resetPassword.page'
+import tData from '../resources/credentials.json'
 
 describe('login page', () => {
-  it('should login with valid credentials', async () => {
+  beforeEach(async () => {
     await HomePage.open()
+    await HomePage.loginButton.click()
+
+    await expect(browser).toHaveUrl(LoginPage.url)
+  })
+
+  afterEach(async () => {
+    await browser.reloadSession()
+  })
+
+  it('should login with valid credentials', async () => {
+    await LoginPage.emailInput.setValue(tData.email)
+    await LoginPage.passwordInput.setValue(tData.password)
+    await LoginPage.loginButton.click()
+
+    await expect(browser).toHaveTitle(DashboardPage.title)
+    await expect(DashboardPage.signedInDirectoryDiv).toBeDisplayed()
+  })
+
+  it('should allow user to submit email for password reset', async () => {
+    await LoginPage.forgotPasswordLink.click()
+
+    await expect(browser).toHaveUrl(ResetPasswordPage.url)
+
+    await ResetPasswordPage.emailInput.setValue(tData.email)
+    await ResetPasswordPage.nextButton.click()
+
+    await expect(ResetPasswordPage.containerHeading).toHaveText('Check your email')
+    await expect(ResetPasswordPage.emailToReceiveResetText).toHaveText(tData.email)
+
+    await ResetPasswordPage.resendEmailButton.click()
+    await expect(ResetPasswordPage.emailSentNotification).toBeDisplayed()
   })
 })
