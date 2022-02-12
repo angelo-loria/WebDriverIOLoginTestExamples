73f99b7b7a5214d94c1b6a0131575b13a79b17b4
angelo-loria <angelosloria@gmail.com>
2022-02-11 18:23:18 -0600

    remove extra args

--- wdio.conf.js
+++ wdio.conf.js
@@ -58,7 +58,7 @@ exports.config = {
     //
     browserName: 'chrome',
     'goog:chromeOptions': {
-      args: ['--headless', '--disable-gpu', '--window-size=1920,1080', '--no-sandbox', '--disable-extensions', '--disable-dev-shm-usage']
+      args: ['--headless', '--disable-gpu', '--window-size=1920,1080', '--disable-extensions']
     },
     acceptInsecureCerts: true
     // If outputDir is provided WebdriverIO can capture driver session logs
